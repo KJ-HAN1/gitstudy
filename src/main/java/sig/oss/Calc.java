0ea9bdb086067995ec009b5f0b9c64e5baf9aaf3
Han,Kukjin <hkjin@kakao.com>
2021-10-04 23:37:40 +0900

    add inc function and test code for it

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,8 +1,15 @@
 public class Calc{
+	private int r=0;
 	public int add(int x, int y){//add method
 		return x+y;
 	}
 	public int sub(int x, int y){
 		return x-y;
+	}
+	public void inc(int d) {
+		r+=d;
+	}
+	public int getResult(){
+		return r;
 	}
 }
